2e6dc822b43037e3cbd5c4c3a9f41e3c19a896aa
Johan Kämpe <kampe.johan@gmail.com>
2016-10-15 17:05:06 +0200

    Added comments and cleaned up code.

--- Kapitel 12/Övning 12.4/struktur_bil_pekare.c
+++ Kapitel 12/Övning 12.4/struktur_bil_pekare.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "bil.h"
+#include "bil.h" //Deklaration av struct bil
 
 /* Övning 12.4, Sida 292
 
@@ -8,24 +8,43 @@ Använd två pekare för att skriva ut informationen från bil-structen.
 */
 
 void skrivUtStructMedPekare(struct bil *p){
-	printf("Registreringsnummer: %s\nFabrikat: %s\nÅrsmodell: %d\nTjänstevikt: %d kg\nMotoreffekt: %d kW", (*p).regNr, (*p).fabrikat, (*p).arsModell, (*p).tjansteVikt, (*p).motorEffekt/1000);
+	printf("Registreringsnummer: %s\n",(*p).regNr);
+	printf("Fabrikat: %s\n", (*p).fabrikat);
+	printf("Årsmodell: %d\n", (*p).arsModell);
+	printf("Tjänstevikt: %d kg\n", (*p).tjansteVikt);
+	printf("Motoreffekt: %d kW", (*p).motorEffekt/1000);
 }
 
 int main(){
-	struct bil bil1 = {.regNr = "SZS361", .fabrikat = "Polaris BIG Boss 6X6", .arsModell = 2002, .tjansteVikt = 420, .motorEffekt = 23000};
-	struct bil bil2 = {.regNr = "SZS351", .fabrikat = "Volvo V70"};
+	//Från övning 12.3:
+	struct bil bil1 = {
+		.regNr = "SZS361",
+		.fabrikat = "Polaris BIG Boss 6X6",
+		.arsModell = 2002,
+		.tjansteVikt = 420,
+		.motorEffekt = 23000
+	};
+
+	struct bil bil2 = {
+		.regNr = "SZS351",
+		.fabrikat = "Volvo V70"
+	};
+
 	bil2.arsModell = 2001;
 	bil2.tjansteVikt = 1610;
 	bil2.motorEffekt = 103000; //103kW
-	printf("Utskrift utan pekare: \n\n");
-	printf("Fordon 1:\nRegistreringsnummer: %s\nFabrikat: %s\nÅrsmodell: %d\nTjänstevikt: %d kg\nMotoreffekt: %d kW", bil1.regNr, bil1.fabrikat, bil1.arsModell, bil1.tjansteVikt, bil1.motorEffekt/1000);
-	printf("\n\nFordon 2:\nRegistreringsnummer: %s\nFabrikat: %s\nÅrsmodell: %d\nTjänstevikt: %d kg\nMotoreffekt: %d kW", bil2.regNr, bil2.fabrikat, bil2.arsModell, bil2.tjansteVikt, bil2.motorEffekt/1000);
-	struct bil *pekare_bil; //Jag använde endast 1 pekare, ville prova hur det fungerar med att sätta den till olika struct
+
+	//Pekare till struct:
+	struct bil *pekare_bil;
+	//Pekaren sätts till addressen för bil1
 	pekare_bil = &bil1;
-	printf("\n\nUtskrift med pekare (och funktion): \n");
+	printf("Utskrift med pekare (och funktion): \n");
+	//Skriver ut värden för bil1 med funktion:
 	skrivUtStructMedPekare(pekare_bil);
+	//Pekaren sätts till addressen för bil2
 	pekare_bil = &bil2;
 	printf("\n\n");
-	skrivUtStructMedPekare(pekare_bil+1);
+	//Skriver ut värden för bil2 med funktion:
+	skrivUtStructMedPekare(pekare_bil);
 	return 0;
 }

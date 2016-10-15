dfc2b45459647b472cdcd2f5f3f20c4344605c6b
Johan Kämpe <kampe.johan@gmail.com>
2016-10-15 14:50:58 +0200

    Added Övning 12.1

---
+++ Kapitel 12/Övning 12.1/struktur_bil.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#inlcude "bil.h";
+
+/* Övning 12.1, Sida 281
+
+Definiera en struktur 'bil', som beskriver bilar
+En bil ska ha registreringsnummer, fabrikat,
+årsmodell, tjänstevikt och motoreffekt.
+
+Välj lämpliga typer för medlemmarna.
+Lägg deklarationen i en egen inkluderingsfil */
+
+int main(){
+	return 0;
+}

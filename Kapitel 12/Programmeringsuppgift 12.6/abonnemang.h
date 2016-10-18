997e645cd450898b753d0c1b5ca92887bf6b52c9
Johan Kämpe <kampe.johan@gmail.com>
2016-10-18 20:17:44 +0200

    Programmeringsuppgift 12.6 completed

--- Kapitel 12/Programmeringsuppgift 12.6/abonnemang.h
+++ Kapitel 12/Programmeringsuppgift 12.6/abonnemang.h
@@ -1,4 +1,16 @@
 #define ABNAMN_LANGD 20
+
+/* Programmeringsuppgift 12.6, Sida 311
+
+Utöka programmet på sida 307 så att man kan 
+ändra abonnemangsuppgifter som redan finns 
+i filen
+
+Tips: Använd fseek för att skriva till rätt position
+i .dat-filen. */
+
+
+//Struktdeklaration av ett telefon/data-abonnemang
 struct abonnemang{
 	char namn[ABNAMN_LANGD];
 	double per_manad;
--- Kapitel 12/Programmeringsuppgift 12.6/abonnemang_andra.c
+++ Kapitel 12/Programmeringsuppgift 12.6/abonnemang_andra.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+//#include <math.h>
 #include <string.h>
 #include "strings_text_v1.h"
 #include "abonnemang.h"
 #define N 100
 #define DATAFIL "mobildata.dat"
 
+/* Programmeringsuppgift 12.6, Sida 311
+
+Utöka programmet på sida 307 så att man kan 
+ändra abonnemangsuppgifter som redan finns 
+i filen
+
+Tips: Använd fseek för att skriva till rätt position
+i .dat-filen. */
+
 void lagg_till(void);
 void lista_ab(void);
 void andra_abonnemang(void);
+//Storleken i byte av en struct abonnemang (64)
+const int strlkStruct = sizeof(struct abonnemang);
 
-int main(){int v;while(1){
+int main(){
+		int v;
+		while(1){
 		printf("1: Lägg till abonnemang\n2: Ändra abonnemang\n3: Lista abonnemang\n0: Avsluta");
 		printf("\n--------------------------------------\nVal: ");
 		scanf("%d", &v);
@@ -33,33 +46,35 @@ int main(){int v;while(1){
 	}
 	return 0;
 }
+
+//Listar abonnemangen i .dat-filen
 void lista_ab(void){
-	struct abonnemang a[N];
-	int n = 0;
+	struct abonnemang a;
+	// "rb" är read binary
 	FILE *f = fopen(DATAFIL, "rb");
-	//Scannar in textfilen till struct
-	while(fread(&a[n], sizeof(struct abonnemang), 1, f) == 1){
-		n++;	//öka typer av abonnemang med 
-	}
-	for(int i=0;i<n;i++){
+	while(fread(&a, strlkStruct, 1, f) == 1){
 		printf("Namn: %s\nPer månad %.2f kr\nGratisminuter: %d\nPris per min: %.2f kr\nFri surf: %.2f GB\nPris per GB: %.2f kr\n\n", 
-		a[i].namn,
-		a[i].per_manad,
-		a[i].fria_min,
-		a[i].extra_min,
-		a[i].fria_gb,
-		a[i].extra_gb
+		a.namn,
+		a.per_manad,
+		a.fria_min,
+		a.extra_min,
+		a.fria_gb,
+		a.extra_gb
 		);
 	}
+	fclose(f);
 }
 
+//Ändra abonnemang
 void andra_abonnemang(void){
-	struct abonnemang a[N];
-	int n = 0, v;
-	FILE *f = fopen(DATAFIL, "rb");
-	//Scannar in textfilen till struct
-	while(fread(&a[n], sizeof(struct abonnemang), 1, f) == 1){
-		n++;	//öka typer av abonnemang med 
+	//a är enbart för att lista namn för abonnemang som finns i filen.
+	struct abonnemang a[N], b;
+	int n = 0, v; // v för val av abonnemang att ändra
+	// r+ är read+write, b=binary
+	FILE *f = fopen(DATAFIL, "r+b");
+	//Läser in datan till struct
+	while(fread(&a[n], strlkStruct, 1, f) == 1){
+		n++; //Räknar antal abonnemang i filen
 	}
 	printf("\nVälj abonnemang att ändra:\n");
 	for(int i=0;i<n;i++){
@@ -67,38 +82,54 @@ void andra_abonnemang(void){
 	}
 	printf("\nVal: ");
 	scanf("%d", &v);
-	printf("Ange ny data för %s:", a[v-1].namn);
-	scanf("%lf%d%lf%lf%lf",
-		&a[v-1].per_manad,
-		&a[v-1].fria_min,
-		&a[v-1].extra_min,
-		&a[v-1].fria_gb,
-		&a[v-1].extra_gb);
-	clearBuffer();
-	freopen(DATAFIL, "wb", f);
-	fseek(f, sizeof(struct abonnemang) * (v-1), SEEK_SET);
-	fwrite(&a, sizeof(struct abonnemang), 1, f);
-}
-
-void lagg_till(void){
-	//Öppna datafilen i binärt läge med append (lägger till text i slutet)
-	FILE *f = fopen(DATAFIL, "ab");
-	//Hållare för inmatat abonneman
-	struct abonnemang b;
-	printf("Ange abonnemangets namn: ");
-	if (!radInput(b.namn, ABNAMN_LANGD)){
-		return; //Avbryter funktionen om användaren matar in EOF
-	}
-	printf("Ange data för %s: ", b.namn);
+	strcpy(b.namn,a[v-1].namn);
+	printf("Ange ny data för %s\n", b.namn);
+	printf("Pris/månad - Gratisminuter/månad - Pris/min - GB/mån - Pris/GB\n");
+	printf("Värden: ");
 	scanf("%lf%d%lf%lf%lf",
 		&b.per_manad,
 		&b.fria_min,
 		&b.extra_min,
 		&b.fria_gb,
 		&b.extra_gb);
-	clearBuffer();
-	//Skriv abonnemanget till datafilen
-	fwrite(&b, sizeof(struct abonnemang), 1, f);
-	//Stäng filen
+	clearBuffer(); //Tömmer textbuffert
+	rewind(f); //Spolar tillbaka filen
+	/*fseek sätter pekarposition i datafilen med utgångspunkt från tredje argumentet
+	SEEK_SET innebär början av filen. Vi stegar lika många byte som (v-1) multiplicerat 
+	med storlek för en struct abonnemang. v sattes när vi valde abonnemang att ändra
+	om det vore det första blir v-1 = 0, vilket gör att vi skriver i början av datafilen */
+	if(!fseek(f, strlkStruct*(v-1), SEEK_SET)){ //fseek returnerar 0 vid lyckat flyttning av  pekarposition
+		printf("Position i fil: %lld \n", ftell(f));
+		fwrite(&b, strlkStruct, 1, f); //Skriv 1 struct till f, vid positionen vi tog fram med fseek
+	}
+	else{
+		printf("Uppdatering av datafil misslyckades!\n");
+	}
+	//Stänger filen - Obs viktigt
 	fclose(f);
+}
+
+void lagg_till(void){
+	//Öppna datafilen i binärt läge med append (lägger till text i slutet)
+	FILE *f = fopen(DATAFIL, "ab");
+	//Hållare för inmatat abonnemang
+	struct abonnemang b;
+	clearBuffer(); //Verkar ligga kvar något efter valinput i mainfunk
+	printf("Ange abonnemangets namn: ");
+	if (!radInput(b.namn, ABNAMN_LANGD)){
+		return; //Avbryter funktionen om användaren matar in EOF
+	}
+	printf("Ange data för %s\n", b.namn);	printf("Pris/månad - Gratisminuter/månad - Pris/min - GB/mån - Pris/GB\n");
+	printf("Värden: ");
+	scanf("%lf%d%lf%lf%lf",
+		&b.per_manad,
+		&b.fria_min,
+		&b.extra_min,
+		&b.fria_gb,
+		&b.extra_gb);
+	//Skriv 1 struct till f i slutet av filen (append)
+	fwrite(&b, strlkStruct, 1, f);
+	//Stänger filen - Obs viktigt
+	fclose(f);
+	return;
 }

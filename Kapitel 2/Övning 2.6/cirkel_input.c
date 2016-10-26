417e1898170f21e2a6495006ea77426097b18ec2
Johan Kämpe <kampe.johan@gmail.com>
2016-10-26 17:19:12 +0200

    Added exercise description in comments & removed unneeded file.

--- Kapitel 2/Övning 2.6/cirkel_input.c
+++ Kapitel 2/Övning 2.6/cirkel_input.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-#define PI 3.14159265359 //sätter "PI" som en konstant
+
+//Definierar "PI" som en konstant
+#define PI 3.14159265359 
+
+/* Övning 2.6, Sida 44
+Skriv om programmet i övning 2.5, använd standardfunktionen scanf
+för att läsa in cirkelns area.
+
+Omkretsen för en cirkel beräknas med 2PIr
+Arean för en cirkel beräknas med PIr^2 */
 
 int main()
 {
@@ -7,7 +16,11 @@ int main()
 	printf("Ange radien för en cirkel: ");
 	scanf("%f", &radie); //iput för användare: radien kan innehålla decimaler
 	//printf kan skriva ut värdet från flera variabler / beräkningar i samma anrop
-	printf("Omkretsen för en cirkel med radien %.3f är %.3f \n", radie, 2 * radie * PI); //Omkretsen för en cirkel beräknas med 2PIr
-	printf("Arean för en cirkel med radien %.3f är %.3f", radie, radie * radie * PI); //Arean för en cirkel beräknas med PIr^2
+	printf("Omkretsen för en cirkel med radien %.3f är %.3f \n", 
+		radie, 
+		2 * radie * PI); 
+	printf("Arean för en cirkel med radien %.3f är %.3f", 
+		radie, 
+		radie * radie * PI);
 	return 0;
 }

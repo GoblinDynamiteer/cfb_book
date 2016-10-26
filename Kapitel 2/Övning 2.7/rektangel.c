1d86271f3cdc1a524536ddc4cc180cfb0eb54c5e
Johan Kämpe <kampe.johan@gmail.com>
2016-10-26 17:23:08 +0200

    Added exercise description in comments

--- Kapitel 2/Övning 2.7/rektangel.c
+++ Kapitel 2/Övning 2.7/rektangel.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-int main ()
-{
+/* Övning 2.7, Sida 46
+
+Skriv ett program som läser in en rektangels höjd och bredd.
+Beräkna och skriv ut dess omkrets och area. */
+
+int main (){
 	float sid1, sid2;
-	printf("Ange rektangels ena sida: ");
+	printf("Ange rektangels höjd: ");
 	scanf("%f", &sid1);
-	printf("Ange rektangels andra sida: ");
+	printf("Ange rektangels bredd: ");
 	scanf("%f", &sid2);
-	printf("\nRektangelns area är: %.3f, dess omkrets är %.3f", sid1 * sid2, 2 * (sid1 + sid2));
+	printf("\nRektangelns area är: %.3f, dess omkrets är %.3f", 
+		sid1 * sid2, 
+		2 * (sid1 + sid2));
 	return 0;
 }
